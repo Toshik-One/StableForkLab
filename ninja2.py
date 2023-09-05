99c914fafce9f6d96dded4cb43a654aea9749dc3
Toshik_One <120157367+Toshik-One@users.noreply.github.com>
2023-09-05 07:49:50 +0300

    Create ninja2.py

---
+++ ninja2.py
@@ -0,0 +1,7 @@
+blasphemy = "stable-diffusion-webui"
+contrlnet = "sd-webui-controlnet"
+import os
+if os.path.exists("/content/gdrive"):
+  storage_path = f"/content/gdrive/{mainpth}"
+else:
+  storage_path = "/content"
