efbf32fb28a5625ee95f1ec607e74165ed674357
Toshik_One <120157367+Toshik-One@users.noreply.github.com>
2023-09-19 12:49:46 +0300

    Delete StableForkLab directory

--- StableForkLab/ninja.py
+++
@@ -1,8 +0,0 @@
-blasphemy = "stable-diffusion-webui"
-StableForkLab = "StableForkLab"
-contrlnet = "sd-webui-controlnet"
-import os
-if os.path.exists("/content/gdrive"):
-  storage_path = f"/content/gdrive/{mainpth}"
-else:
-  storage_path = "/content"
